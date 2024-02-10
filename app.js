fed9e630cc02636fa93dbd0c736e1b8b4fc47742
unknown <waqasahmedwikki15@gmail.com>
2024-02-10 18:53:55 +0500

    table color change with tag

--- app.js
+++ app.js
@@ -4,3 +4,10 @@
 //   baba[i].innerHTML = "poonka";
 // }
 // console.log(document);
+var table1 = document.getElementById("table1");
+var cell = table1.getElementsByTagName("td");
+console.log(cell);
+for (i = 0; i < cell.length; i++) {
+  cell[i].style.color = "yellow";
+  table1.style.backgroundColor = "pink";
+}
